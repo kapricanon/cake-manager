b7917c6c62df2882fdec8b411d5d8baefff6ed73
kapricanon <kapricanon@yahoo.com>
2022-02-01 12:09:49 +0000

    Update git ignore

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CakeList from './CakeList';
 import CakeEdit from "./CakeEdit";
 
@@ -9,11 +9,11 @@ class App extends Component {
   render() {
     return (
         <Router>
-          <Switch>
+          <Route>
             <Route path='/' exact={true} component={Home}/>
             <Route path='/cakes' exact={true} component={CakeList}/>
             <Route path='/cakes/:id' component={CakeEdit}/>
-          </Switch>
+          </Route>
         </Router>
     )
   }
